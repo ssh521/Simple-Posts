319f22a32be9e42e9b18f3d208330d5e586e9937
ssh521 <ssh521@naver.com>
2025-08-27 10:23:07 +0900

    toast ui editor 추가함.

--- resources/views/posts/layout.blade.php
+++ resources/views/posts/layout.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', '간단한 게시판')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- toastui-editor -->
+    <script src="https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js"></script>
+    <link rel="stylesheet" href="https://uicdn.toast.com/editor/latest/toastui-editor.min.css" />
+
 </head>
 <body>
     <div class="container mt-5">
